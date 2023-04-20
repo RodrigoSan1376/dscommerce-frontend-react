bbb04bae3c48d925c3df9907f4a51fc35417a8a3
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-04-20 10:35:55 -0300

    Catalog next page

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -13,6 +13,8 @@ type QueryParams = {
 
 export default function Catalog() {
 
+  const [isLastPage, setIsLastPage] = useState(false);
+
   const [products, setProducts] = useState<ProductDTO[]>([]);
 
   const [queryParams, setQueryParams] = useState<QueryParams>({
@@ -22,12 +24,19 @@ export default function Catalog() {
 
   useEffect(() => {
     productService.findPageRequest(queryParams.page, queryParams.name).then((response) => {
-      setProducts(response.data.content);      
+      const nextPage = response.data.content;
+      setProducts(products.concat(nextPage));
+      setIsLastPage(response.data.last);    
     });
   }, [queryParams]);
 
+  function handleNextPageClick() {
+    setQueryParams({...queryParams, page: queryParams.page + 1})
+  }
+
   function handleSearch(searchText: string) {
-    setQueryParams({...queryParams, name: searchText});
+    setProducts([]);
+    setQueryParams({...queryParams, page: 0, name: searchText});
   }
 
   return (
@@ -41,7 +50,12 @@ export default function Catalog() {
           ))
           }
         </div>
-        <ButtonNextPage />
+        {
+          !isLastPage &&
+          <div onClick={handleNextPageClick}>
+              <ButtonNextPage />
+          </div>
+        }                
       </section>
     </main>
   );
