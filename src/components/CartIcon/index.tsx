21dd2fd4d815c39aad33579160a3eeac48abe07e
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-04-21 20:10:43 -0300

    Context cart count

--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -1,11 +1,19 @@
 import "./styles.css";
 import cartIcon from "../../assets/cart.svg";
+import { useContext, useState } from "react";
+import { ContextCartCount } from "../../utils/context-cart";
 
 export default function CartIcon() {
+
+  const { contextCartCount } = useContext(ContextCartCount);
+
   return (
     <>
       <img src={cartIcon} alt="Carrinho de compras" />
-      <div className="dsc-cart-count">22</div>
+      {
+        contextCartCount > 0 &&
+        <div className="dsc-cart-count">{contextCartCount}</div>
+      }      
     </>
   );
 }
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -3,8 +3,9 @@ import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
 import { useNavigate, useParams, Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
+import { ContextCartCount } from "../../../utils/context-cart";
 import * as productService from "../../../services/product-service";
 import * as cartService from "../../../services/cart-service";
 
@@ -12,6 +13,8 @@ export default function ProductDetails() {
   const params = useParams();
 
   const navigate = useNavigate();
+
+  const { setContextCartCount } = useContext(ContextCartCount);
 
   const [product, setProduct] = useState<ProductDTO>();
 
@@ -28,6 +31,7 @@ export default function ProductDetails() {
   function handleBuyClick() {
     if(product) {
       cartService.addProduct(product);
+      setContextCartCount(cartService.getCart().items.length);
       navigate("/cart");
     }
   }
