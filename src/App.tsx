d1d02ece9f9f0a77e219071d32113d5e41402116
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-04-23 20:57:59 -0300

    Login form, login request

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Catalog from "./routes/ClientHome/Catalog";
 import ProductDetails from "./routes/ClientHome/ProductDetails";
 import ClientHome from "./routes/ClientHome";
 import Cart from "./routes/ClientHome/Cart";
+import Login from "./routes/ClientHome/Login";
 import { useState } from "react";
 import { ContextCartCount } from "./utils/context-cart";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
 
 export default function App() {
   
@@ -17,11 +19,9 @@ export default function App() {
           <Route path="/" element={<ClientHome />}>
             <Route index element={<Catalog />} />
             <Route path="catalog" element={<Catalog />} />
-            <Route
-              path="product-details/:productId"
-              element={<ProductDetails />}
-            />
+            <Route path="product-details/:productId" element={<ProductDetails />} />
             <Route path="cart" element={<Cart />} />
+            <Route path="login" element={<Login />} />
           </Route>
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
