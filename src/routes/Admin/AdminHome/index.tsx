f0aa9a8031b356d5aa3ad837ab34384941b7ebec
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-04-25 21:57:31 -0300

    requestBackend update withCredentials

--- src/routes/Admin/AdminHome/index.tsx
+++ src/routes/Admin/AdminHome/index.tsx
@@ -22,7 +22,7 @@ export default function AdminHome() {
     <main>
       <section id="admin-home-section" className="dsc-container">
         <h2 className="dsc-section-title dsc-mb20">
-          Bem-vindo à área administrativa, {user?.name}
+          Bem-vindo à área administrativa, {user?.name}.
         </h2>
       </section>
     </main>
