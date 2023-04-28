575b100d230c611edc466d345423705b12425c0f
Rodrigo Santana Rodrigues <rodrigosantos1376@gmail.com>
2023-04-28 19:52:12 -0300

    Confirmation

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Cart from "./routes/ClientHome/Cart";
 import Login from "./routes/ClientHome/Login";
 import Admin from "./routes/Admin";
 import AdminHome from "./routes/Admin/AdminHome";
+import Confirmation from "./routes/ClientHome/Confirmation";
 import * as authService from './services/auth-service';
 import * as cartService from './services/cart-service';
 import { useEffect, useState } from "react";
@@ -42,6 +43,7 @@ export default function App() {
               <Route path="product-details/:productId" element={<ProductDetails />} />
               <Route path="cart" element={<Cart />} />
               <Route path="login" element={<Login />} />
+              <Route path="confirmation/:orderId" element={<Confirmation />} />
             </Route>
             <Route path="/admin/" element={<PrivateRoute roles={["ROLE_ADMIN"]}><Admin /></PrivateRoute>} >
               <Route index element={<AdminHome />} />
